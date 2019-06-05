0b69a20a2a2ac0621eb98421a95689398a929648
orshemesh <chikos2011@gmail.com>
2019-06-05 14:23:51 +0300

    COMMIT ALL GUI AND IMAGE

---
+++ Image.py
@@ -0,0 +1,12 @@
+import pygame
+import os
+
+_image_library = {}
+def get_image(path):
+        global _image_library
+        image = _image_library.get(path)
+        if image == None:
+                canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+                image = pygame.image.load(canonicalized_path)
+                _image_library[path] = image
+        return image
--- dronekeyboard.py
+++ dronekeyboard.py
@@ -1,22 +1,24 @@
 import logging
 import sys
-import time
 import pygame
-
+import os
 from dronecontrol import DroneControl
+from Image import get_image
 
 # byte must be in range(0, 256)
 def clamp(n, minn, maxn): return max(min(maxn, n), minn)
-
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting keyboard control app")
     pygame.init()
+    #break the while
     done=False
-    screen = pygame.display.set_mode((400, 300))
+    #screen
+    screen = pygame.display.set_mode((300, 300))
+    #call to DroneControl object
     drone = DroneControl()
+    #call to founction connect
     drone.connect()
 
     airborne = False  # flag denoting whether the drone is airborne
@@ -29,6 +31,9 @@ if __name__ == "__main__":
     t = 128  # up | down
     y = 128  # rotation left | right
 
+    #print
+    screen.blit(get_image('pictures/RSG.jpg'), (0, 0))
+    pygame.display.flip()
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -42,6 +47,9 @@ if __name__ == "__main__":
                     direction = -1
 
                 if key == 27:  # ESC
+                    #print
+                    screen.blit(get_image('pictures/crash.jpg'), (0, 0))
+                    pygame.display.flip()
                     logging.info("Exiting...")
                     drone.stop()
                     pygame.quit()
@@ -49,9 +57,17 @@ if __name__ == "__main__":
 
                 elif key == 32 and event.type == pygame.KEYDOWN:  # spacebar
                     if airborne:
+                        #print
+                        screen.blit(get_image('pictures/land.jpg'), (0, 0))
+                        pygame.display.flip()
+
                         drone.land()
                         airborne = False
                     else:
+                        #print
+                        screen.blit(get_image('pictures/take-off.jpg'), (0, 0))
+                        pygame.display.flip()
+
                         drone.take_off()
                         airborne = True
 
@@ -59,23 +75,51 @@ if __name__ == "__main__":
                     speed_idx += 1
                     if speed_idx == len(speeds):
                         speed_idx = 0
+
+                    #print
+                    screen.blit(get_image('pictures/speed.jpg'), (0, 0))
+                    pygame.display.flip()
                     print("speed: {0}".format(speeds[speed_idx]))
 
                 elif key == 119:  # w
+                    #print
+                    screen.blit(get_image('pictures/up.jpg'), (0, 0))
+                    pygame.display.flip()
                     p += int(direction * 128 * speeds[speed_idx])  # forward
                 elif key == 115:  # s
+                    #print
+                    screen.blit(get_image('pictures/down.png'), (0, 0))
+                    pygame.display.flip()
                     p -= int(direction * 128 * speeds[speed_idx])  # backward
                 elif key == 97:  # a
+                    #print
+                    screen.blit(get_image('pictures/left.jpg'), (0, 0))
+                    pygame.display.flip()
                     r -= int(direction * 128 * speeds[speed_idx])  # left
                 elif key == 100:  # d
+                    #print
+                    screen.blit(get_image('pictures/right.png'), (0, 0))
+                    pygame.display.flip()
                     r += int(direction * 128 * speeds[speed_idx])  # right
                 elif key == 273:  # up arrow
+                    #print
+                    screen.blit(get_image('pictures/take-off.jpg'), (0, 0))
+                    pygame.display.flip()
                     t += int(direction * 128 * speeds[speed_idx])  # up
                 elif key == 274:  # down arrow
+                    #print
+                    screen.blit(get_image('pictures/land.jpg'), (0, 0))
+                    pygame.display.flip()
                     t -= int(direction * 128 * speeds[speed_idx])  # down
                 elif key == 275:  # right arrow
+                    #print
+                    screen.blit(get_image('pictures/rotate right.png'), (0, 0))
+                    pygame.display.flip()
                     y += direction * 128  # rotation right
                 elif key == 276:  # left arrow
+                    #print
+                    screen.blit(get_image('pictures/rotate left.jpg'), (0, 0))
+                    pygame.display.flip()
                     y -= direction * 128  # rotation left
 
 
